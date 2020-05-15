34d780eedf058907aab1c071cd841a0b01d4f069
Dirk Strauss <dstrauss@ds-2.de>
2020-05-15 13:53:54 +0200

    TM-000: output address

--- aws_db_encr/output.tf
+++ aws_db_encr/output.tf
@@ -1,7 +1,11 @@
 output "endpoint" {
-    value=aws_db_instance.db.endpoint
+  value = aws_db_instance.db.endpoint
 }
 
 output "arn" {
-    value=aws_db_instance.db.arn
+  value = aws_db_instance.db.arn
+}
+
+output "address" {
+  value = aws_db_instance.db.address
 }
