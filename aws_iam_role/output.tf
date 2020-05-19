44d02f8ee1672e912c46dfbf32b6aab3c4d054ef
Dirk Strauss <dstrauss@ds-2.de>
2020-05-19 17:21:51 +0200

    TM-000: allow null????

--- aws_iam_role/output.tf
+++ aws_iam_role/output.tf
@@ -1,15 +1,15 @@
 output "arn" {
-  value = aws_iam_role.role.arn
+  value = aws_iam_role.role != null ? aws_iam_role.role.arn : null
 }
 
 output "name" {
-  value = aws_iam_role.role.name
+  value = aws_iam_role.role != null ? aws_iam_role.role.name : null
 }
 
 output "unique_id" {
-  value = aws_iam_role.role.unique_id
+  value = aws_iam_role.role != null ? aws_iam_role.role.unique_id : null
 }
 
 output "id" {
-  value = aws_iam_role.role.id
+  value = aws_iam_role.role != null ? aws_iam_role.role.id : null
 }
