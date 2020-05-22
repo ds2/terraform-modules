903bfc59fef51c7fe8cec5ef0309008e825072a9
Dirk Strauss <dstrauss@ds-2.de>
2020-05-22 12:17:03 +0200

    TM-2: add idea

--- github_project/main.tf
+++ github_project/main.tf
@@ -33,3 +33,26 @@ resource "github_team_repository" "teams" {
   repository = github_repository.project.name
   permission = "push"
 }
+
+# resource "github_branch_protection" "protect_master" {
+#   repository     = github_repository.project.name
+#   branch         = "master"
+#   enforce_admins = true
+
+#   required_status_checks {
+#     strict   = false
+#     contexts = ["ci/travis"]
+#   }
+
+#   required_pull_request_reviews {
+#     dismiss_stale_reviews = true
+#     dismissal_users       = ["foo-user"]
+#     dismissal_teams       = ["${github_team.example.slug}", "${github_team.second.slug}"]
+#   }
+
+#   restrictions {
+#     users = ["foo-user"]
+#     teams = ["${github_team.example.slug}"]
+#     apps  = ["foo-app"]
+#   }
+# }
