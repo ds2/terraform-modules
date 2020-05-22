4379086f05ae4a7adec0ff03f68498566a8f097d
Dirk Strauss <dstrauss@ds-2.de>
2020-05-22 13:07:12 +0200

    TM-2: also add team

---
+++ github_team/vars.tf
@@ -0,0 +1,28 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = null
+}
+
+variable "isSecret" {
+  type    = bool
+  default = true
+}
+
+variable "parentTeamId" {
+  type    = string
+  default = null
+}
+
+variable "members" {
+    type=set(string)
+    default=[]
+}
+
+variable "maintainers" {
+    type=set(string)
+    default=[]
+}
--- tests/github/tests.tf
+++ tests/github/tests.tf
@@ -8,12 +8,19 @@ terraform {
   required_version = ">= 0.12"
 }
 
-module "repo1" {
-  source              = "../../github_project"
-  name                = "infra001-test-bucket-20200520"
-  topics              = ["test", "maven", "java"]
-  initialize          = true
-  isPrivate           = false
-  projectLicenseId    = "agpl-3.0"
-  gitignoreTemplateId = "Gradle"
-}
+# module "team1" {
+#   source      = "../../github_team"
+#   name        = "infra001-test-team1_20200520"
+#   description = "a test team"
+#   members     = ["ds2ci"]
+# }
+
+# module "repo1" {
+#   source              = "../../github_project"
+#   name                = "infra001-test-bucket-20200520"
+#   topics              = ["test", "maven", "java"]
+#   initialize          = true
+#   isPrivate           = false
+#   projectLicenseId    = "agpl-3.0"
+#   gitignoreTemplateId = "Gradle"
+# }
