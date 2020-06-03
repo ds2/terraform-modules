491dff1a510f01388d9b77ea49c2d3fa5333c5af
Dirk Strauss <dstrauss@ds-2.de>
2020-06-03 13:01:08 +0200

    TM-000: fix issue with gateway

--- aws_vpc_network/main.tf
+++ aws_vpc_network/main.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "vpc" {
   cidr_block                       = var.cidr
   assign_generated_ipv6_cidr_block = var.enableIpv6
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
   tags = {
     Terraformed = true
     Name        = var.name
@@ -10,6 +12,16 @@ resource "aws_vpc" "vpc" {
 resource "aws_egress_only_internet_gateway" "egress_only" {
   vpc_id = aws_vpc.vpc.id
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = var.name
+    Terraformed = true
+  }
+}
+
 
 resource "aws_subnet" "private" {
   count                           = length(var.availZones)
@@ -46,4 +58,30 @@ resource "aws_subnet" "public" {
   }
   # lifecycle { create_before_destroy = true }
   depends_on = [aws_vpc.vpc]
+}
+
+resource "aws_route_table" "pubroute" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    # ipv6_cidr_block="::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.egress_only.id
+  }
+
+  tags = {
+    Name        = var.name
+    Terraformed = true
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.pubroute.id
 }
