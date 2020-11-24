9a54389585ad2b3e1df1e1434ef23e6dcbbdf5d7
Dirk Strauss <dstrauss@ds-2.de>
2020-11-24 18:14:55 +0100

    TM-00: add ec2 test

--- tests/aws/aws_tests.tf
+++ tests/aws/aws_tests.tf
@@ -139,16 +139,16 @@ module "aws_s3_test" {
 #   # publicPull=true
 # }
 
-# module "ec2_test" {
-#   source              = "../../aws_ec2_instance"
-#   name                = "infra-test-20200103"
-#   amiId               = "ami-0b90a8636b6f955c1"
-#   sshKeyName          = module.kp_test.name
-#   subnetId            = module.vpc_test.private_subnet_ids[0]
-#   dnsDomain           = "n8w8.app."
-#   dnsName             = "tenebron"
-#   # unlimitedCpuCredits = false
-# }
+module "ec2_test" {
+  source              = "../../aws_ec2_instance"
+  name                = "infra-test-20200103"
+  amiId               = "ami-0b90a8636b6f955c1"
+  sshKeyName          = module.kp_test.name
+  subnetId            = module.vpc_test.private_subnet_ids[0]
+  dnsDomain           = "n8w8.app."
+  dnsName             = "tenebron"
+  # unlimitedCpuCredits = false
+}
 
 module "lambda_test" {
   source     = "../../aws_lambda_func"
