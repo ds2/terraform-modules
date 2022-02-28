80064d8dfcf310911148036fb37fd47b4264872b
Dirk <472167-dstrauss@users.noreply.gitlab.com>
2022-02-28 10:01:38 +0100

    TM-000: do not yet enforce TF client versions

--- bitbucket_project/versions.tf
+++ bitbucket_project/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     bitbucket = {
-      source  = "andsafe-ag/bitbucket"
+      source = "andsafe-ag/bitbucket"
       # version = "2.5.0"
     }
   }
-  required_version = "~> 1.0"
+  # required_version = "~> 1.0"
 }
--- bitbucket_repository/versions.tf
+++ bitbucket_repository/versions.tf
@@ -4,5 +4,5 @@ terraform {
       source  = "andsafe-ag/bitbucket"
     }
   }
-  required_version = "~> 1.0"
+  # required_version = "~> 1.0"
 }
