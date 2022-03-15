16089965c55de675eb5b4c3aa49eb88761bbea08
Dirk <472167-dstrauss@users.noreply.gitlab.com>
2022-03-15 09:58:27 +0100

    - add idea for terraform-docs

--- az_resgrp/main.tf
+++ az_resgrp/main.tf
@@ -1,3 +1,8 @@
+/**
+ * # Azure Resourcegroup
+ * 
+ * to easily setup an azure resourcegroup via terraform.
+*/
 resource "azurerm_resource_group" "resgrp" {
   name     = var.id
   location = var.location
