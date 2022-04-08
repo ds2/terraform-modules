d3c69590bbe39866ca6f901fcda44a6e28968a1f
Dirk <472167-dstrauss@users.noreply.gitlab.com>
2022-04-08 14:23:00 +0200

    - use latest gl plugin

--- tests/gitlab/tests.tf
+++ tests/gitlab/tests.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     gitlab = {
       source  = "gitlabhq/gitlab"
-      version = "~> 3.6.0"
+      version = "~> 3.0"
     }
   }
   required_version = "~> 1.1.0"
