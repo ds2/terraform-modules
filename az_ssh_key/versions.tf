6b0c230eee8257b9b553005abab784a114236c91
Lexxy <472167-dstrauss@users.noreply.gitlab.com>
2025-05-26 15:28:53 +0200

    chore: allow greater versions for Azure providers

--- az_ssh_key/versions.tf
+++ az_ssh_key/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">=3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~>4.0"
+      version = ">=4.0"
     }
   }
 }
